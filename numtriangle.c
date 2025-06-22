56f46afe3d9c0a0f3fe6d7f7167550c52ed98936
ndeshpan30 <nidhi.deshpan@gmail.com>
2025-06-22 21:48:14 +0530

    adding program 6i

--- numtriangle.c
+++ numtriangle.c
@@ -3,20 +3,20 @@ int main()
 {
   int n,k=1,x,j,i,p;
   
-  for(j=1;j<=4;j++)
+  for(j=1;j<=4;j++) //where j is the row number.
     {
-    p=4-j;
+    p=4-j;//p is the number of spaces to be printed before the starting number of each row
     
-      for(x=p;x>0;x--)
+      for(x=p;x>0;x--)//printf spaces
 	{
 	  printf(" ");
 	}
-      for(i=1;i<=j;i++)
+      for(i=1;i<=j;i++)//prints the numbers
 	{
 	  printf("%d ",k);
-	  k++;	  
+	  k++;//increments the value of k so that each time the loop runs, it prints the next number, and not the same one.	  
 	}
-      printf(" \n");
+      printf(" \n");//to move to the next line
 
    
     }
