5f9c865027ce01e7df798f00fbb2f6b5bcaf4f81
ndeshpan30 <nidhi.deshpan@gmail.com>
2025-08-19 22:23:10 +0530

    program to print any square matrix in a spiral sequence

---
+++ spiral.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+int main()
+{
+  int i=0,j=0,n;
+  int s[4][4];
+
+  printf("Enter the number of rows and columns in the square matrix: ");
+  scanf("%d",&n);
+  for(i=0;i<=n-1;i++)
+    {
+      for(j=0;j<=n-1;j++)
+	{
+	  printf("Enter the elements of row %d, column %d: ",i,j);
+	  scanf("%d",&s[i][j]);
+
+	}
+      
+    }
+  printf("The matrix looks like this: \n");
+  for(i=0;i<=n-1;i++)
+    {
+      for(j=0;j<=n-1;j++)
+	{
+	  printf(" %d ",s[i][j]);
+
+	}
+      printf("\n");
+    }
+
+
+
+
+  printf("Spiral form: ");
+  
+  int top=0;
+  int bottom=n-1;
+  int left=0;
+  int right=n-1;
+
+  while(left<=right &&top<=bottom)
+    {
+  for(j=left;j<=right;j++)
+    {
+      printf("%d ",s[top][j]);
+    }
+  top=top+1;
+  for(i=top;i<=bottom;i++)
+    {
+      printf("%d ",s[i][right]);
+    }
+  right=right-1;
+  for(j=right;j>=left;j--)
+    {
+      printf("%d ",s[bottom][j]);
+    }
+  bottom=bottom-1;
+  for(i=bottom;i>=top;i--)
+    {
+      printf("%d ",s[i][left]);
+    }
+  left=left+1;
+    }
+  return 0;
+}
